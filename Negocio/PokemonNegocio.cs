a6696def7bff1f6aa46777a1d3d4ed43293e4fd2
Julieta <jubarraza5@gmail.com>
2024-09-06 15:56:23 -0300

    creacion capa negocio

--- Negocio/PokemonNegocio.cs
+++ Negocio/PokemonNegocio.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Dominio;
 
-namespace App_Pokemon
+namespace Negocio
 {
-    internal class PokemonNegocio
+    public class PokemonNegocio
     {
 
         public List<Pokemon> Listar() 
